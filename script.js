03efa315aeabd756efb6b3b6e9f8bde01a2e2b04
N-NJ <171683125+N-NJ@users.noreply.github.com>
2024-08-11 15:14:34 +0300

    Added Randomized colors per cell functionality, adjusted changeBgColor for it

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ let gridSize = 256;
 let pixelSideLength = Math.sqrt(
   Math.pow(mainContainer.clientWidth, 2) / gridSize
 );
-let strokeColor;
+let strokeColor = "black";
 let pixelDimension = `width: ${pixelSideLength}px; height: ${pixelSideLength}px`;
+let mode = "fixed";
 function setGridSize(e) {
   let userSizeInput = prompt("Please enter your desired grid scale");
   gridSize = Math.pow(userSizeInput, 2);
@@ -43,7 +44,7 @@ function changeBgColor(e) {
     target.setAttribute(
       "style",
       pixelDimension +
-        `; background-color:${strokeColor ? strokeColor : "black"}`
+        `; background-color:${mode == "fixed" ? strokeColor : randomColor()}`
     );
     target.setAttribute("id", id);
   } else {
@@ -59,14 +60,43 @@ function clearPage() {
   const divCells = mainContainer.querySelectorAll("div");
   divCells.forEach((item) => item.setAttribute("style", pixelDimension));
 }
-function changeColor() {
-  strokeColor = colorButton.value;
+function changeColor(e) {
+  let target = e.target;
+  console.log(target);
+
+  if (target.id == "randomColor") {
+    mode = "random";
+  } else {
+    strokeColor = colorButton.value;
+    mode = "fixed";
+  }
 }
 function randomColor() {
-  let randomColor = Math.random() * 1001;
-  strokeColor = `#`;
+  let hexArray = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  let randomColor = [];
+  for (let n = 0; n < 6; n++) {
+    randomColor.push(hexArray[Math.floor(Math.random() * 16)]);
+  }
+  return (strokeColor = `#${randomColor.join("")}`);
 }
 mainContainer.addEventListener("mouseover", changeBgColor);
 clearButton.addEventListener("click", clearPage);
-colorButton.addEventListener("change", changeColor);
-randomColorButton.addEventListener("click", randomColor);
+colorButton.addEventListener("change", (e) => changeColor(e));
+randomColorButton.addEventListener("click", (e) => changeColor(e));
